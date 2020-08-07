6ac65a882cc5edecc5be6ac1ddbd50bdf4e7b49a
PraveenDhakad <praveendhakad47@gmail.com>
2020-08-07 23:06:44 +0530

    Add Single Movie Post Data

--- inc/parts/single/peliculas.php
+++ inc/parts/single/peliculas.php
@@ -132,25 +132,72 @@ if(cs_get_option('dynamicbg') == true) { ?>
 
         <!-- Movie more info -->
         <div id="info" class="sbox">
-            <!-- Single Movie Post Data -->
-            <h1 style="text-align: center;"><strong><span style="font-family: 'book antiqua', palatino, serif;"><span style="color: #ff0000; font-size: 18pt;">Download <span style="font-family: georgia, palatino, serif;"><?php the_title(); ?> </span></span><span style="color: #ff0000; font-size: 18pt;"> Full Movie In HD 1080p/720p</span></span></strong></h1>
+
+<!-- Single Movie Post Data -->
+<h1 style="text-align: center;"><strong><span style="font-family: 'book antiqua', palatino, serif;"><span style="color: #ff0000; font-size: 18pt;">Download <span style="font-family: georgia, palatino, serif;"><?php the_title(); ?> </span></span><span style="color: #ff0000; font-size: 18pt;"> Full Movie In HD 1080p/720p</span></span></strong></h1>
             <p><span style='color: rgb(245, 62, 198); font-family: "Times New Roman", Times, serif; font-size: 17px;'><strong>Also Known As : <?php the_title(); ?> -</strong>
-            </span><span style='font-family: "Times New Roman", Times, serif; font-size: 17px;'> You Can Always <strong>download <?php the_title(); ?> Movie</strong> in Full HD – Every film fast to your Own PC And Mobile. Latest Movie One <?php the_title(); ?> Download, <strong>Link Of The Download In Bottom In 720p & 1080p Quality</strong>.
+            </span><span style='font-family: "Times New Roman", Times, serif; font-size: 17px;'> You Can Always <strong> download <?php the_title(); ?> Movie</strong> in Full HD – Every film fast to your Own PC And Mobile. Latest Movie One <?php the_title(); ?>Download, <strong>Link Of The Download In Bottom In 720p & 1080p Quality</strong>.
            </span></p><br>
-           <p style="text-align: center;"><span style="font-size: 12pt; color: #ff0000; font-family: 'book antiqua', palatino, serif;"><strong>Share with Your Friends If you like our Website :-)</strong></span><br><span style="font-size: 12pt; color: #ff0000; font-family: 'book antiqua', palatino, serif;"><strong>Don&rsquo;t Forget to Bookmark Our Website :-)</strong></span> </p><br>
-            <img class="aligncenter wp-image-36" alt="<?php the_title(); ?>" width="328" height="492" src="<?php echo get_the_post_thumbnail(); ?><br>
+         
+            <img class="aligncenter wp-image-36" alt="<?php the_title(); ?>" width="328" height="492" src="<?php echo get_the_post_thumbnail(); ?><br><p style="text-align: center;"><span style="font-size: 12pt; color: #ff0000; font-family: 'book antiqua', palatino, serif;"><strong>Share with Your Friends If you like our Website :-)</strong></span><br><span style="font-size: 12pt; color: #ff0000; font-family: 'book antiqua', palatino, serif;"><strong>Don&rsquo;t Forget to Bookmark Our Website :-)</strong></span> </p><br><br>
             <div style='font-family: "Times New Roman", Times, serif; font-size: 17px;' itemprop="description" class="wp-content">
-            <span style="font-family: georgia, palatino, serif;"><span style="color: #ff0000;"><strong>Description</strong> </span>: <?php the_content(); ?></span><hr>
-                <p style="text-align: center;"><span style="font-family: georgia, palatino, serif; font-size: 14px;"><strong><span style="color: #3366ff;"><?php the_title(); ?> Movie Download</span>. I Hope You Like Our Website <a href="<?php echo site_url(); ?>"><?php echo site_url(); ?></a></strong></span></p><hr>
-                <p style="text-align: left;"><span style="font-family: georgia, palatino, serif;"><strong><span style="font-size: 14px;">Title:</span></strong><span style="font-size: 14px;">&nbsp;<?php the_title(); ?></span></span><span style="font-size: 14px;"><br><span style="font-family: georgia, palatino, serif;"><strong>Genre:&nbsp;</strong><!-- Grnre List --><strong>&nbsp;</strong></span><br><span style="font-family: georgia, palatino, serif;"><strong>Release Date:</strong>&nbsp;<?php if($d = doo_isset($postmeta,'release_date')) echo "<span class='date'>".doo_date_compose($d,false)."</span>"; ?></span><br><span style="font-family: georgia, palatino, serif;"><strong>Trailer:</strong> Watch</span><br><span style="font-family: georgia, palatino, serif;"><strong>RUNTIME:&nbsp;Not Available </strong></span><br><span style="font-family: georgia, palatino, serif;"><strong>Category:&nbsp;</strong><a href="<?php echo site_url('/genre/hollywood-movie/') ?>" rel="noopener noreferrer" target="_blank">Hollywood Movies</a></span><br><span style="font-family: georgia, palatino, serif;"><strong>Language:&nbsp;</strong>Dual Audio</span><br><span style="font-family: georgia, palatino, serif;"><strong>Information Source:</strong> WIKIPEDIA</span><br><span style="font-family: georgia, palatino, serif;"><strong>IMDB:&nbsp;https://www.imdb.com/title/......../</strong></span><br><span style="font-family: georgia, palatino, serif;"><strong><span style="color: #00ff00;">Subtitles</span>:</strong> Download Movie Subtitles (Arabic, English, Hindi &hellip;..) <?php the_title(); ?> Subtitles For Free Here.</span><br><span style="font-family: georgia, palatino, serif;"><span style="color: #ff00ff;"><strong>Skip Directly To Download:</strong></span> <a href="<?php echo site_url(); ?>" rel="noopener noreferrer" target="_blank">Click Here</a></span>&nbsp;</span></p>
-                <hr>
-<p style="text-align: center;"><span style="font-family: georgia, palatino, serif;"><strong><a href="<?php echo site_url(); ?>"><span style="font-size: 16px;">Bollywood Movies, Hollywood Movies, Hindi Dubbed Movies, Punjabi Movies</span></a></strong></span></p>
+            <span style="font-family: georgia, palatino, serif;"><span style="color: #ff0000;"></span> <?php the_content(); ?></span><hr>
+                <p style="text-align: center;"><span style="font-family: georgia, palatino, serif; font-size: 14px;"><strong><span style="color: #3366ff;"><?php the_title(); ?> Movie Download</span>. I Hope You Like Our Website <a href="<?php echo site_url(); ?>"><?php echo site_url(); ?></a></strong></span></p><hr>
+                
+                <a href="//graizoah.com/afu.php?zoneid=2489978" target="_blank" rel="nofollow noopener"><img class="size-full wp-image-143 aligncenter" src="https://o2movies.site/wp-content/uploads/2020/01/info-image-min.png" alt="Movie Info" width="278" height="94" /></a>
+                
+                
+                                <p style="text-align: left;"><span style="font-family: georgia, palatino, serif;"><strong><span style="font-size: 14px;">Title:</span></strong><span style="font-size: 14px;">&nbsp;<?php the_title(); ?></span></span><span style="font-size: 14px;"><br><span style="font-family: georgia, palatino, serif;"><strong>Genre:&nbsp;</strong><!-- Grnre List --><strong>&nbsp;</strong></span><br><span style="font-family: georgia, palatino, serif;"><strong>Release Date:</strong>&nbsp;<?php if($d = doo_isset($postmeta,'release_date')) echo "<span class='date'>".doo_date_compose($d,false)."</span>"; ?></span><br><span style="font-family: georgia, palatino, serif;"><strong>Trailer:</strong> Watch</span><br><span style="font-family: georgia, palatino, serif;"><strong>RUNTIME:&nbsp;Not Available </strong></span><br><span style="font-family: georgia, palatino, serif;"><strong>Category:&nbsp;</strong><a href="<?php echo site_url('/genre/hollywood-movie/') ?>" rel="noopener noreferrer" target="_blank">Not Available</a></span><br><span style="font-family: georgia, palatino, serif;"><strong>Language:&nbsp;</strong>Dual Audio</span><br><span style="font-family: georgia, palatino, serif;"><strong>Information Source:</strong> WIKIPEDIA</span><br><span style="font-family: georgia, palatino, serif;"><strong>IMDB:&nbsp;https://www.imdb.com/title/......../</strong></span><br><span style="font-family: georgia, palatino, serif;"><strong><span style="color: #00ff00;">Subtitles</span>:</strong> Download Movie Subtitles (Arabic, English, Hindi &hellip;..) <?php the_title(); ?> Subtitles For Free Here.</span><br><span style="font-family: georgia, palatino, serif;"><span style="color: #ff00ff;"><strong>Skip Directly To Download:</strong></span> <a href="<?php echo site_url(); ?>" rel="noopener noreferrer" target="_blank">Click Here</a></span>&nbsp;</span></p><hr>
+                                
+                                <a title="Click Here To Download" href="//graizoah.com/afu.php?zoneid=2489978" target="_blank" rel="nofollow noopener"><img class="aligncenter wp-image-138 size-full" src="https://o2movies.site/wp-content/uploads/2020/01/new-download-link-min.png" alt="Click Here TO Download" width="300" height="112" /></a>
+                                
+                                <p style="text-align: center;"><span style="font-family: georgia, palatino, serif;"><strong><a href="<?php echo site_url(); ?>"><span style="font-size: 16px;">Bollywood Movies, Hollywood Movies, Hindi Dubbed Movies, Punjabi Movies</span></a></strong></span></p>
 <p style="text-align: center;"><span style='font-family: "book antiqua", palatino, serif; font-size: 24px;'><?php the_title(); ?> Trailer :</span></p>
+<!-- Youtube Video Embed URL -->
+<?php
+$youtubeVideo = get_field('youtube_video_url');
+if ( !empty ( $youtubeVideo) ):
+         ?>
+        <div class="embed-container">
+    <?php the_field('youtube_video_url'); ?>
+</div>
+<?php endif; ?>
+<style>
+    .embed-container { 
+        position: relative; 
+        padding-bottom: 56.25%;
+        overflow: hidden;
+        max-width: 100%;
+        height: auto;
+       
+    } 
+
+    .embed-container iframe,
+    .embed-container object,
+    .embed-container embed { 
+        position: absolute;
+        
+        top: 0;
+        left: 9%;
+        width: 85%;
+        height: 85%;
+    }
+</style>
+
 <p style="text-align: center;"><span style="font-family: georgia, palatino, serif; color: #ff00ff;"><strong><span style="font-size: 16px;">PLEASE Click ON Images For Original Screen Size</span></strong></span></p>
+
+<?php 
+$image = get_field('image');
+if( !empty( $image ) ): ?>
+    <img class="size-full wp-image-39681 aligncenter" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+<?php endif; ?>
+
 <p style="text-align: center;"><span style='font-size: 16px; color: rgb(255, 0, 0); font-family: "book antiqua", palatino, serif;'><strong>Share with Your Friends If you like our Website :-)</strong></span><span style="font-size: 16px;"><br></span><span style='font-size: 16px; color: rgb(255, 0, 0); font-family: "book antiqua", palatino, serif;'><strong>Don&rsquo;t Forget to Bookmark Our Website :-)</strong></span></p>
 <p style="text-align: center;"><span style="font-family: georgia, palatino, serif;"><span class="html-attribute-value" style="font-size: 16px;"><?php the_title(); ?> &ndash; Full Movie | FREE DOWNLOAD | TORRENT | HD 1080p | x264 | WEB-DL | DD5.1 | H264 | MP4 | 720p | DVD | Bluray.</span></span></p>
 <p><br></p>
 
+<a href="//graizoah.com/afu.php?zoneid=2489978" target="_blank" rel="nofollow noopener"><img class="aligncenter wp-image-39687 size-medium" src="https://o2movies.site/wp-content/uploads/2020/08/130-1300704_click-here-to-download-green-button-click-here-300x102.jpg" alt="Click Here To Download Movie" width="300" height="102" /></a>
+<!-- Single Movie Post Data End Here -->
                 
                 <?php if($images = doo_isset($postmeta, 'imagenes')) { ?>
                 <div id="dt_galery" class="galeria">
